7d8164ee90106aa6c2239f1a0b88952c0cbda138
Davidoar15 <davidoar15@gmail.com>
2023-10-25 20:26:04 -0300

    add/getCountries

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+  import { onMounted, ref } from "vue";
   import PageHeader from './components/PageHeader.vue';
+  import axiosClient from "./utils/axios";
+  import { Country } from "./types/country";
+
+  const countries = ref<Country[]>([]);
+
+  const getCountries = async () => {
+    try {
+      const { data } = await axiosClient.get("/all");
+      countries.value = data;
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
+  onMounted(() => {
+    getCountries();
+  });
 </script>
 
 <template>
   <PageHeader />
+  <div 
+    v-for="country in countries"
+  >
+    {{ country.name.common }}
+  </div>
 </template>
 
 <!-- <style scoped>
